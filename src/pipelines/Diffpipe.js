d592317a62e4ab544f1afde948d1dd17376f8b57
donghyeon kim <dhygns@naver.com>
2017-06-09 16:36:37 +0900

    opt algorithm update

--- src/pipelines/Diffpipe.js
+++ src/pipelines/Diffpipe.js
@@ -22,33 +22,66 @@ class Diffpipe extends PIPE{
 
         varying vec2 vtex;
 
+        const float lambda = 0.002;
+        const float thres = 0.1;
+
         void main(void) {
           vec2 offset_x = vec2(1.0, 0.0) / unif_reso.x;
           vec2 offset_y = vec2(0.0, 1.0) / unif_reso.y;
 
-          //현재의 색깔
-          vec3 currcol = texture2D(unif_curr, vtex).rgb;
-          vec3 prevcol = texture2D(unif_prev, vtex).rgb;
-          vec3 diffcol = prevcol - currcol;
+          vec4 sd = texture2D(unif_curr, vtex) - texture2D(unif_prev, vtex);
+          vec4 gx =
+            texture2D(unif_prev, vtex + offset_x) - texture2D(unif_prev, vtex - offset_x) +
+            texture2D(unif_curr, vtex + offset_x) - texture2D(unif_curr, vtex - offset_x);
+          vec4 gy =
+            texture2D(unif_prev, vtex + offset_y) - texture2D(unif_prev, vtex - offset_y) +
+            texture2D(unif_curr, vtex + offset_y) - texture2D(unif_curr, vtex - offset_y);
 
-          float power = smoothstep(0.0, 0.5, length(diffcol));
+          vec4 gd = max(vec4(lambda) * 2.0, sqrt((gx * gx) + (gy * gy) * vec4(lambda)));
 
-          float mdiff = length(vec3(1.0));
-          vec3 diffr = texture2D(unif_prev, vtex + offset_x).rgb - currcol;
-          vec3 diffl = texture2D(unif_prev, vtex - offset_x).rgb - currcol;
-          vec3 difft = texture2D(unif_prev, vtex + offset_y).rgb - currcol;
-          vec3 diffb = texture2D(unif_prev, vtex - offset_y).rgb - currcol;
+          vec4 vx = sd * (gx / gd);
+          vec4 vy = sd * (gy / gd);
 
-          vec2 dir = vec2(0.0);
-          dir.x += mdiff - length(diffr);
-          dir.x -= mdiff - length(diffl);
-          dir.y += mdiff - length(difft);
-          dir.y -= mdiff - length(diffb);
+          vec2 flow = vec2(0.0);
+          flow.x = (vx.r + vx.g + vx.b) / 3.0;
+          flow.y = (vy.r + vy.g + vy.b) / 3.0;
 
-          vec2 force = power * normalize(dir);
+          float str = length(flow);
+          if(str * thres > 0.0) {
+            if(str < thres) {
+              flow = vec2 (0.0);
+            }
+            else {
+              str = (str - thres) / ( 1.0 - thres);
+              flow = normalize(flow) * str;
+            }
+          }
+          gl_FragColor = vec4(flow * 0.5 + 0.5, 0.0 , 1.0);
 
-          if(length(dir) < 0.01) force = vec2(0.0, 0.0);
-          gl_FragColor = vec4(force * 0.5 + 0.5 , 0.0, 1.0);
+          //
+          // //현재의 색깔
+          // vec3 currcol = texture2D(unif_curr, vtex).rgb;
+          // vec3 prevcol = texture2D(unif_prev, vtex).rgb;
+          // vec3 diffcol = prevcol - currcol;
+          //
+          // float power = smoothstep(0.0, 0.5, length(diffcol));
+          //
+          // float mdiff = length(vec3(1.0));
+          // vec3 diffr = texture2D(unif_prev, vtex + offset_x).rgb - currcol;
+          // vec3 diffl = texture2D(unif_prev, vtex - offset_x).rgb - currcol;
+          // vec3 difft = texture2D(unif_prev, vtex + offset_y).rgb - currcol;
+          // vec3 diffb = texture2D(unif_prev, vtex - offset_y).rgb - currcol;
+          //
+          // vec2 dir = vec2(0.0);
+          // dir.x += mdiff - length(diffr);
+          // dir.x -= mdiff - length(diffl);
+          // dir.y += mdiff - length(difft);
+          // dir.y -= mdiff - length(diffb);
+          //
+          // vec2 force = power * normalize(dir);
+          //
+          // if(length(dir) < 0.01) force = vec2(0.0, 0.0);
+          // gl_FragColor = vec4(force * 0.5 + 0.5 , 0.0, 1.0);
         }
 
         `,
